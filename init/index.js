95d67d7c4bd8efa38039ff3bf642d2d68c05454d
P Rishabh Kumar <rishabh260405@gmail.com>
2025-12-27 15:38:53 +0530

    Second attempt at fixing listing render issue

--- init/index.js
+++ init/index.js
@@ -1,47 +1,53 @@
 // This contains the complete database initialization code
 
-const mongoose = require('mongoose');
-const Listing = require("../models/listings.js"); // ✅ FIXED
+const mongoose = require("mongoose");
+const Listing = require("../models/listings.js");
 const data = require("./data.js");
-const { object } = require('joi');
+const { object } = require("joi");
 
-const url = "mongodb://127.0.0.1:27017/wanderly";
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
+
+const url = process.env.CLOUD_MONGODB_DATABASE_LINK;
 
 createConnection()
-.then(() => {
+  .then(() => {
     console.log("Connected successfully with the database !!!!");
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log(`This error occurred in connecting with the database : ${err}`);
-});
+  });
 
 async function createConnection() {
-    await mongoose.connect(url);
+  await mongoose.connect(url);
 }
 
 // adding the data to the database
 
-
-const initializeDatabase = async ()=>{
-    // first we will clean any uneccessary data from the database
-    Listing.deleteMany({})
-    .then(async (res)=>{
-        console.log("Database cleaning completed !!!")
-        console.log(res)
-        data.data = data.data.map((obj)=>({...obj, owner: "687bd6c3ef91023c18399449"}))
-        await Listing.insertMany(data.data)
-        .then((res)=>{
-            console.log("Sample data insertion completed !!!!")
-            console.log(res)
+const initializeDatabase = async () => {
+  // first we will clean any uneccessary data from the database
+  Listing.deleteMany({})
+    .then(async (res) => {
+      console.log("Database cleaning completed !!!");
+      console.log(res);
+      data.data = data.data.map((obj) => ({
+        ...obj,
+        owner: "687bd6c3ef91023c18399449",
+      }));
+      await Listing.insertMany(data.data)
+        .then((res) => {
+          console.log("Sample data insertion completed !!!!");
+          console.log(res);
         })
-        .catch((err)=>{
-            console.log(`The sample data could not be inserted due to this error : ${err}`)
-        })
+        .catch((err) => {
+          console.log(
+            `The sample data could not be inserted due to this error : ${err}`
+          );
+        });
     })
-    .catch((err)=>{
-        console.log(`Database could not be cleaned due to this error : ${err}`)
-    })
-}
+    .catch((err) => {
+      console.log(`Database could not be cleaned due to this error : ${err}`);
+    });
+};
 
-
-initializeDatabase()
+initializeDatabase();
